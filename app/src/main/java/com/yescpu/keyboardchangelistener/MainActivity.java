98161f04d162b99181251de83920f50cb5436095
yes.cpu@gmail.com <yes.cpu@gmail.com>
2019-07-15 11:23:02 +0800

    Fixed bugs and optimization demo

--- app/src/main/java/com/yescpu/keyboardchangelistener/MainActivity.java
+++ app/src/main/java/com/yescpu/keyboardchangelistener/MainActivity.java
@@ -1,65 +1,38 @@
 package com.yescpu.keyboardchangelistener;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.yescpu.keyboardchangelib.KeyboardChangeListener;
 
-public class MainActivity extends AppCompatActivity implements KeyboardChangeListener.KeyBoardListener {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements KeyboardChangeListener.KeyboardListener {
     private static final String TAG = "MainActivity";
     private KeyboardChangeListener mKeyboardChangeListener;
+    private EditText mEdContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        mKeyboardChangeListener = new KeyboardChangeListener(this);
-        mKeyboardChangeListener.setKeyBoardListener(this);
+        mEdContent = findViewById(R.id.edContent);
+        mKeyboardChangeListener = KeyboardChangeListener.create(MainActivity.this);
+        mKeyboardChangeListener.setKeyboardListener(MainActivity.this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onKeyboardChange(boolean isShow, int keyboardHeight) {
-        Log.d(TAG, "onKeyboardChange() called with: " + "isShow = [" + isShow + "], keyboardHeight = [" + keyboardHeight + "]");
+        String text = String.format(Locale.getDefault(), "isShow %s, height=%d", String.valueOf(isShow), keyboardHeight);
+        mEdContent.setText(text);
+        mEdContent.setSelection(text.length());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mKeyboardChangeListener.destroy();
     }
 }
